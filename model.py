361f96b67711f2e555a3b38b7bfabd3a32616df2
Vibhor Agarwal <agarwalvibhor84@gmail.com>
2018-08-05 16:41:36 +0530

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from numpy import array
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-# from keras.utils.vis_utils import plot_model
+from keras.utils.vis_utils import plot_model
 from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
@@ -15,9 +15,11 @@ from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
 from keras.callbacks import ModelCheckpoint
 
+
 # load a clean dataset
 def load_clean_sentences(filename):
 	return load(open(filename, 'rb'))
+
 
 # fit a tokenizer
 def create_tokenizer(lines):
@@ -25,9 +27,11 @@ def create_tokenizer(lines):
 	tokenizer.fit_on_texts(lines)
 	return tokenizer
 
+
 # max sentence length
 def max_length(lines):
 	return max(len(line.split()) for line in lines)
+
 
 # encode and pad sequences
 def encode_sequences(tokenizer, length, lines):
@@ -36,6 +40,7 @@ def encode_sequences(tokenizer, length, lines):
 	# pad sequences with 0 values
 	X = pad_sequences(X, maxlen=length, padding='post')
 	return X
+
 
 # one hot encode target sequence
 def encode_output(sequences, vocab_size):
@@ -47,6 +52,7 @@ def encode_output(sequences, vocab_size):
 	y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
 	return y
 
+
 # define NMT model
 def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
 	model = Sequential()
@@ -56,6 +62,7 @@ def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
 	model.add(LSTM(n_units, return_sequences=True))
 	model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
 	return model
+
 
 # load datasets
 dataset = load_clean_sentences('english-german-both.pkl')
@@ -68,6 +75,7 @@ eng_vocab_size = len(eng_tokenizer.word_index) + 1
 eng_length = max_length(dataset[:, 0])
 print('English Vocabulary Size: %d' % eng_vocab_size)
 print('English Max Length: %d' % (eng_length))
+
 # prepare german tokenizer
 ger_tokenizer = create_tokenizer(dataset[:, 1])
 ger_vocab_size = len(ger_tokenizer.word_index) + 1
@@ -79,6 +87,7 @@ print('German Max Length: %d' % (ger_length))
 trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
 trainY = encode_sequences(eng_tokenizer, eng_length, train[:, 0])
 trainY = encode_output(trainY, eng_vocab_size)
+
 # prepare validation data
 testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
 testY = encode_sequences(eng_tokenizer, eng_length, test[:, 0])
@@ -89,7 +98,7 @@ model = define_model(ger_vocab_size, eng_vocab_size, ger_length, eng_length, 256
 model.compile(optimizer='adam', loss='categorical_crossentropy')
 # summarize defined model
 print(model.summary())
-# plot_model(model, to_file='model.png', show_shapes=True)
+plot_model(model, to_file='model.png', show_shapes=True)
 # fit model
 filename = 'model.h5'
 checkpoint = ModelCheckpoint(filename, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
